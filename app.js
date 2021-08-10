219e789fc1b3114935724a53bb08f236cee8cc6d
brianbianchi <bianchibrianj@gmail.com>
2021-08-09 22:00:26 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const qr = require("qrcode");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use("/img", express.static('img'));
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("index", { src: undefined, message: "" });
+});
+
+app.post("/", (req, res) => {
+    const { type } = req.body;
+    let qrData = '';
+
+    switch (type) {
+        case 'url':
+            qrData = req.body.url;
+            break;
+        case 'phone':
+            const { phone } = req.body;
+            qrData = `tel: ${phone}`;
+            break;
+        case 'sms':
+            const { sms, smsMessage } = req.body;
+            qrData = `SMSTO: ${sms}`;
+            if (smsMessage) {
+                qrData += `:${smsMessage}`;
+            }
+            break;
+        case 'wifi':
+            const { auth, name, password } = req.body;
+            if (auth === 'none') {
+                qrData = `WIFI:S:${name};`;
+            } else {
+                qrData = `WIFI:T:${auth};S:${name};P:${password};`;
+            }
+            break;
+        case 'email':
+            const { email, emailSubject } = req.body;
+            qrData = `mailto: ${email}`;
+            if (emailSubject) {
+                qrData += `?subject=${encodeURIComponent(emailSubject)}`
+            }
+            break;
+        case 'contact':
+            // const { event, eventStart, eventEnd } = req.body;
+            qrData = `
+BEGIN:VCARD
+N:Smith;John;
+TEL;TYPE=work,VOICE:(111) 555-1212
+TEL;TYPE=home,VOICE:(404) 386-1017
+TEL;TYPE=fax:(866) 408-1212
+EMAIL:smith.j@smithdesigns.com
+ORG:Smith Designs LLC
+TITLE:Lead Designer
+ADR;TYPE=WORK,PREF:;;151 Moore Avenue;Grand Rapids;MI;49503;United States of America
+URL:https://www.smithdesigns.com
+VERSION:3.0
+END:VCARD
+`;
+            break;
+        case 'event':
+            const { eventOrganizerName, eventOrganizerEmail, eventTitle, eventStart, eventEnd } = req.body;
+            qrData = `
+BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+ORGANIZER;CN=${eventOrganizerName}:MAILTO:${eventOrganizerEmail}
+DTSTART:${formatTimestring(new Date(eventStart).toISOString())}
+DTEND:${formatTimestring(new Date(eventEnd).toISOString())}
+SUMMARY:${eventTitle}
+END:VEVENT
+END:VCALENDAR
+`;
+            break;
+        default:
+            break;
+    }
+
+    if (!qrData.length) {
+        res.render("index", { src: undefined, message: "No data to create QR with." });
+        return;
+    }
+
+    qr.toDataURL(qrData, (err, src) => {
+        if (err) res.render("index", { src: undefined, message: err });;
+
+        res.render("index", { src, message: "" });
+    });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
+
+/**
+ * Date.toISOString() => yyyymmddThhmmssZ
+ * ISO 8601
+ */
+function formatTimestring(s) {
+    return b = s.replace(/-/gi, '').replace(/:/gi, '').replace('.', '').replace('000Z', 'Z');
+}
